469325e6c93a105f2df0926a293cab64e9c004f8
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-01 14:22:09 +0700

    part 4

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -8,3 +8,8 @@ int pilihan;
 // Function untuk cek bilangan prima
 bool isPrima(int n) {
     if (n <= 1) return false;
+
+    int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+             return false;
