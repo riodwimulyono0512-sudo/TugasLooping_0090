c2978921e8a84b6c6d188e658609c1dcd3bb5519
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-04-01 14:23:56 +0700

    part 7

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -22,3 +22,7 @@ bool isPrima(int n) {
 // Function untuk cek bilangan Fibonacci
 bool isFibonacci(int n) {
     int a = 0, b = 1, c = 0;
+
+    while (c <= n) {
+        if (c == n) {
+            return true;
